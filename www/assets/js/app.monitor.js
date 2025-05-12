d1ce7dc8970ac1617f984f966aaa80d2ebc9a4d9
SebastianP03 <jsalfonsop03@gmail.com>
2025-05-12 16:12:26 -0500

    dev

--- www/assets/js/app.monitor.js
+++ www/assets/js/app.monitor.js
@@ -10,13 +10,31 @@ app.monitor = {
         
         console.log(data);
 
+        // Preparar datos antes de pasarlos al template
+        const processedData = data.map(item => {
+            const allOk = item.d.every(dd => dd.ge !== "0");
+            return {
+                ...item,
+                radicarOK: allOk
+            };
+        });
+
+
         const tableTemplate = `
             <div class="card mt-4 ml-5">
                 <div class="card-header">
                 <div class="icon-title">${iconProcess}</div>  Control Armado Cuentas Médicas - Diario
                 </div>
                 <div class="card-body">
-                <input id="txtSearch" type="text" class="form-control input-search mb-3" placeholder="Buscar ..." autocomplete="off" style="width: 980px;">
+                <div class="d-flex align-items-center mb-3">
+                    <input id="txtSearch" type="text" class="form-control" placeholder="Buscar ..." autocomplete="off" style="width: 780px; height: 36px;">
+                    <select id="selectFilter" class="form-select" style="width: 200px; height: 36px;">
+                        <option value="all">Mostrar todos</option>
+                        <option value="ok">Radicar OK</option>
+                        <option value="no">Radicar Pendientes</option>
+                    </select>
+                </div>
+
                 <div id="searchTarget">
                     <style>
                     .table-robot-armado-cuenta {
@@ -51,7 +69,11 @@ app.monitor = {
                         <tbody>
                         <tr>
                             <td class="text-center">
-                            <div class="rounded-circle bg-danger d-inline-block" style="width: 25px; height: 25px;"></div>
+                                {{? d.radicarOK }}
+                                    <div class="rounded-circle bg-success d-inline-block" style="width: 25px; height: 25px;"></div>
+                                {{??}}
+                                    <div class="rounded-circle bg-danger d-inline-block" style="width: 25px; height: 25px;"></div>
+                                {{?}}
                             </td>
                             <td class="text-start fw-bold">{{=d.f}}</td>
                             <td class="text-center">{{=d.fe}}</td>
@@ -97,9 +119,99 @@ app.monitor = {
                 </div>
                 </div>
             </div>
-            `;
+        `;
 
-        const html = doT.template(tableTemplate)({ detail: data });
+        const tableTemplate = `
+            <div class="card mt-4 ml-5">
+                <div class="card-header">
+                    <div class="icon-title">${iconProcess}</div>  Control Armado Cuentas Médicas - Diario
+                </div>
+                <div class="card-body">
+                    <select id="selectFilter" class="form-select mb-2" style="width: 200px;">
+                        <option value="all">Mostrar todos</option>
+                        <option value="ok">Radicar OK</option>
+                        <option value="no">Radicar Pendientes</option>
+                    </select>
+                    <input id="txtSearch" type="text" class="form-control input-search mb-3" placeholder="Buscar ..." autocomplete="off" style="width: 980px;">
+                    <div id="searchTarget">
+                        <style>
+                            .table-robot-armado-cuenta, .table-robot-armado-cuenta-detail {
+                                table-layout: fixed;
+                                width: 930px;
+                            }
+                            .table-robot-armado-cuenta-detail tr th {
+                                font-size: 11px;
+                            }
+                        </style>
+
+                        {{~it.detail: d:id}}
+                        <div class="search-group mb-4" data-radicar="{{=d.radicarOK ? 'ok' : 'no'}}">
+                            <table class="table table-bordered table-sm table-robot-armado-cuenta">
+                                <colgroup><col width="50"><col width="350"><col width="200"><col width="140"><col width="240"></colgroup>
+                                <thead class="table-primary">
+                                    <tr>
+                                        <th class="text-center">Radicar</th>
+                                        <th class="text-center">Factura</th>
+                                        <th class="text-center">Fecha</th>
+                                        <th class="text-center">Valor</th>
+                                        <th class="text-center">Paciente</th>
+                                        <th class="text-center"></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td class="text-center">
+                                            {{? d.radicarOK }}
+                                                <div class="rounded-circle bg-success d-inline-block" style="width: 25px; height: 25px;"></div>
+                                            {{??}}
+                                                <div class="rounded-circle bg-danger d-inline-block" style="width: 25px; height: 25px;"></div>
+                                            {{?}}
+                                        </td>
+                                        <td class="text-start fw-bold">{{=d.f}}</td>
+                                        <td class="text-center">{{=d.fe}}</td>
+                                        <td class="text-end">$ {{=numberDecimal.format(d.v)}}</td>
+                                        <td colspan="2" class="text-center">{{=d.p}}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+
+                            <table class="table table-bordered table-sm table-robot-armado-cuenta-detail mb-3">
+                                <colgroup><col width="400"><col width="200"><col width="140"><col width="120"><col width="120"></colgroup>
+                                <thead class="table-primary">
+                                    <tr>
+                                        <th class="text-center">Grupo</th>
+                                        <th class="text-center">Soporte</th>
+                                        <th class="text-center">Armado</th>
+                                        <th class="text-center">Estado</th>
+                                        <th class="text-center">Descripción</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {{~d.d: dd:idd}}
+                                    <tr>
+                                        <td class="text-start">{{=dd.g}}</td>
+                                        <td class="text-start">{{=dd.s}}</td>
+                                        <td class="text-center">
+                                            {{? dd.ge == "0"}}
+                                                <div class="rounded-circle bg-danger d-inline-block" style="width: 25px; height: 25px;"></div>
+                                            {{??}}
+                                                <div class="rounded-circle bg-success d-inline-block" style="width: 25px; height: 25px;"></div>
+                                            {{?}}
+                                        </td>
+                                        <td class="text-start">{{=dd.o}}</td>
+                                        <td class="text-start">{{=dd.n}}</td>
+                                    </tr>
+                                    {{~}}
+                                </tbody>
+                            </table>
+                        </div>
+                        {{~}}
+                    </div>
+                </div>
+            </div>
+        `;
+
+        const html = doT.template(tableTemplate)({ detail: processedData });
         const container = document.getElementById("container");
 
         if (container) {
